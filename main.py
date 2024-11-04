1eac1137f985c366029a4b46a8bc0fea932e92b6
imseano <soreta@csu.fullerton.edu>
2024-11-04 15:30:00 -0800

    Reformatted code into PDFViewer class.

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from pypdf import PdfReader
 from pdf2image import convert_from_path
 
-def main():
-    print('hello world')
+class PdfViewer:
+    def __init__(self, filepath):
+        self.pdfpath = filepath
+
+        self.pdf = PdfReader(self.pdfpath)
+        self.num_pages = len(self.pdf.pages)
+
+        self.first_page = self.pdf.get_page(0)
+        self.page_images = convert_from_path(self.pdfpath)
+
+    def openFile(self):
+        print("hi")
+
+    def getPDFImage(self, index):
+        return self.page_images[index]
+
+
+
+ 
+def main(): 
     pdfPath = 'examples/Example-1.pdf'
-    reader = PdfReader(pdfPath)
-    print(reader.get_num_pages())
 
-    pageObj = reader.get_page(0);
+    test = PdfViewer(pdfPath)
+    print(test.pdf.get_num_pages())
 
-    print(pageObj.extract_text());
+    test.getPDFImage(0).show()
 
-    page_image = convert_from_path(pdfPath)
-    page_image[0].show()
-
-
+        
 main()
+
