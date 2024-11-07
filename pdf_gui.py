3db76b0582ceddaa6f76f1b17ec508e904dcb620
/home/richard/.gitconfig <richardtrinh195@csu.fullerton.edu>
2024-11-07 15:38:03 -0800

    Added menu bar with dummy commands.

--- pdf_gui.py
+++ pdf_gui.py
@@ -7,8 +7,24 @@ class PdfGUI:
     def __init__(self, master):
         self.master = master
         self.master.title('Basic PDF Viewer')
+        self.master.geometry('854x480')
+        
+        menu_bar = Menu(self.master)
+        
+        file_menu = Menu(menu_bar, tearoff = 0)
+        menu_bar.add_cascade(label = 'File', menu = file_menu)
+        file_menu.add_command(label = 'Open File', command = None)
+        file_menu.add_separator()
+        file_menu.add_command(label = 'Exit', command = self.master.destroy)
+        
+        help_menu = Menu(menu_bar, tearoff = 0)
+        menu_bar.add_cascade(label = 'Help', menu = help_menu)
+        help_menu.add_command(label = 'Help', command = None)
+        help_menu.add_command(label = 'About', command = None)
+        
+        self.master.config(menu = menu_bar)
     
-    def open():
+    def open_file():
         pass
     
 
