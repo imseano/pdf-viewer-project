d53311473c5b95397bb5497c7e289f0620ea9bd9
Richard Trinh <richardtrinh195@csu.fullerton.edu>
2024-11-11 15:40:34 -0800

    Updated variables in __init__ for usability in class functions. Example shown with update_page_label.

--- pdf_gui.py
+++ pdf_gui.py
@@ -18,59 +18,59 @@ class PdfGUI:
         self.master.geometry('854x480')
         
         # Menu bar creation
-        menu_bar = Menu(self.master)
+        self.menu_bar = Menu(self.master)
         
         # File menu commands
-        file_menu = Menu(menu_bar, tearoff = 0)
-        menu_bar.add_cascade(label = 'File', menu = file_menu)
+        self.file_menu = Menu(self.menu_bar, tearoff = 0)
+        self.menu_bar.add_cascade(label = 'File', menu = self.file_menu)
         
         ### TO-DO: Add command for the Open File command in the file menu. ###
-        file_menu.add_command(label = 'Open File', command = None)
+        self.file_menu.add_command(label = 'Open File', command = None)
         ### END OF TO-DO. ###
-        file_menu.add_separator()
-        file_menu.add_command(label = 'Exit', command = self.master.destroy)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label = 'Exit', command = self.master.destroy)
         
         # Help menu commands
-        help_menu = Menu(menu_bar, tearoff = 0)
-        menu_bar.add_cascade(label = 'Help', menu = help_menu)
+        self.help_menu = Menu(self.menu_bar, tearoff = 0)
+        self.menu_bar.add_cascade(label = 'Help', menu = self.help_menu)
         ### TO-DO: Add commands for the 'Help' and 'About' menu commands in the help menu. ###
-        help_menu.add_command(label = 'Help', command = None)
-        help_menu.add_command(label = 'About', command = None)
+        self.help_menu.add_command(label = 'Help', command = None)
+        self.help_menu.add_command(label = 'About', command = None)
         ### END OF TO-DO. ###
         
         # Configure and display the menu bar.
-        self.master.config(menu = menu_bar)
+        self.master.config(menu = self.menu_bar)
         
         # Task bar creation
-        taskbar = Frame(self.master, bg = "lightgrey", height = 30)
-        taskbar.pack(side=TOP, fill=X)
+        self.taskbar = Frame(self.master, bg = "lightgrey", height = 30)
+        self.taskbar.pack(side=TOP, fill=X)
         
         # Task bar buttons
         ### TO-DO: Add the appropriate commands to the task bar buttons. ###
-        previous_button = Button(taskbar, text = "<<", command = None)
-        previous_button.pack(side=LEFT, padx=2, pady=2)
+        self.previous_button = Button(self.taskbar, text = "<<", command = None)
+        self.previous_button.pack(side=LEFT, padx=2, pady=2)
         
-        next_button = Button(taskbar, text = ">>", command = None)
-        next_button.pack(side=LEFT, padx=2, pady=2)
+        self.next_button = Button(self.taskbar, text = ">>", command = None)
+        self.next_button.pack(side=LEFT, padx=2, pady=2)
     
-        zoom_in_button = Button(taskbar, text = "+", command = None)
-        zoom_in_button.pack(side=RIGHT, padx=2, pady=2)  
+        self.zoom_in_button = Button(self.taskbar, text = "+", command = None)
+        self.zoom_in_button.pack(side=RIGHT, padx=2, pady=2)  
         
-        zoom_out_button = Button(taskbar, text = "-", command = None)
-        zoom_out_button.pack(side=RIGHT, padx=2, pady=2)  
+        self.zoom_out_button = Button(self.taskbar, text = "-", command = None)
+        self.zoom_out_button.pack(side=RIGHT, padx=2, pady=2)  
         
-        page_label = Label(taskbar, text=f"Page {current_page} of {total_pages}")
-        page_label.pack(side=LEFT, padx=2, pady=2)
+        self.page_label = Label(self.taskbar, text=f"Page {current_page} of {total_pages}")
+        self.page_label.pack(side=LEFT, padx=2, pady=2)
         
-        zoom_label = Label(taskbar, text=f"Zoom: {int(zoom_level * 100)}%")
-        zoom_label.pack(side=RIGHT, padx=2, pady=2)
+        self.zoom_label = Label(self.taskbar, text=f"Zoom: {int(zoom_level * 100)}%")
+        self.zoom_label.pack(side=RIGHT, padx=2, pady=2)
         
         # Comment: More task buttons can be added later.
         ### END OF TO-DO. ###
         
         # Canvas creation to display PDF document pages.
-        canvas = Canvas(self.master, bg="white")
-        canvas.pack(side=LEFT, fill=BOTH, expand=True)
+        self.canvas = Canvas(self.master, bg="white")
+        self.canvas.pack(side=LEFT, fill=BOTH, expand=True)
     
     ### TO-DO: Add other necessary functions needed for each menu bar command or task bar button we need. ###
     # Note: Place holder functions have already been created. You just need to add the code that will get the job done.
@@ -94,6 +94,10 @@ class PdfGUI:
     
     def zoom_out():
         pass
+    
+    def update_page_label(self):
+        self.page_label.config(text=f"Page {current_page} of {total_pages}")
+        self.zoom_label.config(text=f"Zoom: {int(zoom_level * 100)}%")
     ### END OF TO-DO. ###
     
 # Root window and mainloop to keep the window open.
