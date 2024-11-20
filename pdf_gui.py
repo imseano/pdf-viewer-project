183a6ef19f61f0ac4b6c876a6c0043dd4c5ace4d
/home/richard/.gitconfig <richardtrinh195@csu.fullerton.edu>
2024-11-19 18:12:23 -0800

    Added code for previous_page and next_page and did some cleanup.

--- pdf_gui.py
+++ pdf_gui.py
@@ -48,10 +48,10 @@ class PdfGUI:
         
         # Task bar buttons
         ### TO-DO: Add the appropriate commands to the task bar buttons. ###
-        self.previous_button = Button(self.taskbar, text = "<<", command = None)
+        self.previous_button = Button(self.taskbar, text = "<<", command = self.previous_page)
         self.previous_button.pack(side=LEFT, padx=2, pady=2)
         
-        self.next_button = Button(self.taskbar, text = ">>", command = None)
+        self.next_button = Button(self.taskbar, text = ">>", command = self.next_page)
         self.next_button.pack(side=LEFT, padx=2, pady=2)
     
         self.zoom_in_button = Button(self.taskbar, text = "+", command = None)
@@ -72,7 +72,7 @@ class PdfGUI:
         # Canvas creation to display PDF document pages.
         self.canvas = Canvas(self.master, width=854, height=480, bg="white")
         self.canvas.pack(side=LEFT, fill=BOTH, expand=True)
-        self.display_page(0, pdf_file)
+        self.display_page(0)
 
     # Takes a PIL Image and displays it on the canvas
     def load_image(self, image):
@@ -94,13 +94,12 @@ class PdfGUI:
             total_pages = pdf_file.num_pages
             images = pdf_file.page_images
             current_page = 1
-            self.display_page(current_page - 1, pdf_file)
-            self.update_page_label()
+            self.display_page(current_page - 1)
 
     # Displays a single page to the GUI.
     # Updates page label and zoom based on whether the zoom buttons and previous page or next_page button is used.
-    def display_page(self, page_number, pdf_file):
-        global current_page, total_pages, zoom_level
+    def display_page(self, page_number):
+        global current_page, total_pages, zoom_level, pdf_file
         current_page = page_number + 1
         self.update_page_label()
 
@@ -109,11 +108,17 @@ class PdfGUI:
         
 
     
-    def previous_page():
-        pass
+    def previous_page(self):
+        global current_page, total_pages
+        if current_page > 1:
+            current_page -= 1
+            self.display_page(current_page - 1)
     
-    def next_page():
-        pass
+    def next_page(self):
+        global current_page, total_pages
+        if current_page < total_pages:
+            current_page += 1
+            self.display_page(current_page - 1)
     
     def jump_to_page():
         pass
